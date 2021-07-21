6682a9bffc4ca1840745bc5877df033a2ceda28d
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-07-21 13:06:27 +0200

    Basics template form done

--- src/app/template/basics/basics.component.ts
+++ src/app/template/basics/basics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,13 +9,42 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicsComponent implements OnInit {
 
+  initForm={
+    product: 'Google Nest',
+    price: 59,
+    stock: 100
+  }
+
+  @ViewChild('myForm') myForm!: NgForm
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  save(myForm:NgForm){
-    console.log(myForm)
+  validateName():boolean{
+    let isValid: boolean=false;
+    isValid=this.myForm?.controls.product?.invalid && this.myForm?.controls.product?.touched;
+    return isValid;
+  }
+
+  validatePrice():boolean{
+    let isValid: boolean=false;
+    isValid=this.myForm?.controls.price?.touched && this.myForm?.controls.price?.value<0;
+    return isValid; 
+
+
+  }
+
+
+
+  save(){
+    console.log(this.myForm)
+
+    this.myForm.resetForm({
+      price: "0",
+      stock: "0"
+    });
   }
 
 }
