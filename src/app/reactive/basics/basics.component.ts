b5209773d0c7e23aad8b01cb929cdf5cc30c34fb
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-07-24 20:40:47 +0200

    Started to work with reactive basics form

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BasicsComponent{
+
+  myForm:FormGroup=new FormGroup({
+    'name': new FormControl('Google Nest'),
+    
+  });
 
   constructor() { }
 
