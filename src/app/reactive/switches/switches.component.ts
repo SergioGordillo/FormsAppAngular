266ba3a15a1d05ca7ef0969b10748390657158c3
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-07-26 14:10:50 +0200

    Default values added to switches reactive form

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  myForm:FormGroup=this.fb.group({
+    gender:['M', Validators.required],
+    notifications:[true, Validators.required],
+    termsAndConditions:[false, Validators.requiredTrue]
+  })
 
-  ngOnInit(): void {
+  person={
+    gender: 'F',
+    notifications: true
+  }
+
+  constructor(private fb:FormBuilder) { }
+
+  ngOnInit(){
+    this.myForm.reset({...this.person, termsAndConditions:true});
   }
 
 }
